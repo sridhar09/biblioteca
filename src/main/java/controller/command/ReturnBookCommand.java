ae4dd9547da6a3dcbb651a0e81ddb98f77c734b0
Sridhar Mundra <sridhar.mundra@thoughtworks.com>
2018-09-23 23:14:49 +0530

    Completed Story 12, Release 1

--- src/main/java/controller/command/ReturnBookCommand.java
+++ src/main/java/controller/command/ReturnBookCommand.java
@@ -7,12 +7,16 @@ import view.OutputDriver;
 public class ReturnBookCommand implements Command {
     private static final String INPUT_BOOK_NAME_MESSAGE= "Book title:\n";
     private static final String SUCCESSFUL_RETURN_MESSAGE = "Thank you for returning the book.";
+    private static final String UNSUCCESSFUL_RETURN_MESSAGE = "That is not a valid book to return.";
     @Override
     public void perform(Library library, InputDriver inputDriver, OutputDriver outputDriver) {
         outputDriver.println(INPUT_BOOK_NAME_MESSAGE);
         String returnBook = inputDriver.readString();
         if(library.returnItem(returnBook)){
             outputDriver.println(SUCCESSFUL_RETURN_MESSAGE);
+        }
+        else{
+            outputDriver.println(UNSUCCESSFUL_RETURN_MESSAGE);
         }
     }
 }
--- src/test/java/controller/command/ReturnBookCommandTest.java
+++ src/test/java/controller/command/ReturnBookCommandTest.java
@@ -25,7 +25,7 @@ class ReturnBookCommandTest {
 
     @Test
     @DisplayName("Should print Success Message when checkout successful")
-    void testCheckoutTrue(){
+    void testReturnTrue(){
         Library mockLibrary = mock(Library.class);
         OutputDriver outputDriver = mock(OutputDriver.class);
         InputDriver inputDriver = mock(InputDriver.class);
@@ -33,6 +33,18 @@ class ReturnBookCommandTest {
         when(mockLibrary.returnItem("Book1")).thenReturn(true);
         new ReturnBookCommand().perform(mockLibrary,inputDriver,outputDriver);
         verify(outputDriver).println("Thank you for returning the book.");
+    }
+
+    @Test
+    @DisplayName("Should print Success Message when checkout successful")
+    void testeturnTrue(){
+        Library mockLibrary = mock(Library.class);
+        OutputDriver outputDriver = mock(OutputDriver.class);
+        InputDriver inputDriver = mock(InputDriver.class);
+        when(inputDriver.readString()).thenReturn("Book1");
+        when(mockLibrary.returnItem("Book1")).thenReturn(false);
+        new ReturnBookCommand().perform(mockLibrary,inputDriver,outputDriver);
+        verify(outputDriver).println("That is not a valid book to return.");
     }
 
 }
